8f0bdb2c31940a57c1600abbacbf951893f71854
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-04-28 02:16:14 -0700

    Added content (certifications)
    Inside certification I added colors and boxes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  let dode = "_blank";
+  let certifications = ["AWS", "Goole Cloud", "Microsoft Azure"];
   return (
     <>
-      <header className="App-header">
-        <h1 className='App-logo'>
-        imported to Git.
+      <header className="background">
+        <h1 className='titleBox'>
+          <div className='titleText'>
+            Machine Learning Path Plan
+          </div>
+        </h1>
+        <h1 className='content'>
+          Python<br/>
+          ML course<br/>
+          projects
+          {certifications}
         </h1>
       </header>
     </>
