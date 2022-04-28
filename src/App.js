6a75ac75df69fedb574c7e1f1a2dd5bf0b20168f
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-04-28 01:38:54 -0700

    First init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import logo from './logo.svg';
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  let dode = "_blank";
+  return (
+    <>
+      <header className="App-header">
+        <h1 className='App-logo'>
+        imported to Git.
+        </h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
